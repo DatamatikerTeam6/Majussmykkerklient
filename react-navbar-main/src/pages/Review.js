b204b05bf843db22dfdf7784ab71ba03b0f06467
Justin <jakj54082@edu.ucl.dk>
2024-10-31 13:45:12 +0100

    First push

---
+++ react-navbar-main/src/pages/Review.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Review() {
+    return
+    (
+        <div>
+         <h1>Stjerner</h1>
+         <img src= "./Images/logo.png"/>
+         </div>
+    )  
+    
+}
